4238ae20acf1d4168493ef52fd9cbecf71c10b3a
Jonathan SIMONNEY <jonathan.simonney@supinternet.fr>
2018-01-04 17:00:40 +0100

    fosUserBundle WORKS! (apparently at least...)

---
+++ src/Entity/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    // add your own fields
+}
