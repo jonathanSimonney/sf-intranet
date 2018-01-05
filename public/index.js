437c35309033d6d39755e6f17e67709c064b5631
Jonathan SIMONNEY <jonathan.simonney@supinternet.fr>
2018-01-05 09:39:56 +0100

    added style for the different pages, but realised REGISTER IS NOT FUNCTIONAL. Also, don't know if webpack is necessary, but don't want to remove it now it is there. Maybe it will have to be changed afterwards though.

---
+++ public/index.js
@@ -0,0 +1,9 @@
+$('.fixed-action-btn').floatingActionButton({
+    direction: 'top', // Direction menu comes out
+    hoverEnabled: true, // Hover enabled
+    toolbarEnabled: false // Toolbar transition enabled
+  });
+
+$(document).ready(function(){
+    $('select').select();
+});
