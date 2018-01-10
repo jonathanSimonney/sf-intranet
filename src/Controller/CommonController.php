41e09a395f261cb759e2630bd94d86970f0b28e3
Jonathan SIMONNEY <jonathan.simonney@supinternet.fr>
2018-01-10 10:25:49 +0100

    not tested, but subject list should be all right (but only students have action.)

--- src/Controller/CommonController.php
+++ src/Controller/CommonController.php
@@ -6,10 +6,7 @@ use App\Entity\Subject;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Psr\Log\InvalidArgumentException;
-use Symfony\Component\Config\Definition\Exception\Exception;
-use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/common")
@@ -30,13 +27,30 @@ class CommonController extends Controller
      * @Route("/subject", name="subject_index")
      * @Method("GET")
      */
-    public function indexSubjectAction(Subject $subject)
+    public function indexSubjectAction()
     {
-        $user = $this->get('security.token_storage')->getToken()->getUser();
-
-        if (!$user) {
-            throw new \Exception('Vous n\'êtes pas connecté');
+        $arrayFormAssignment = [];
+        $em = $this->getDoctrine()->getManager();
+        if ($this->getUser()->getMaxRole() === 'ROLE_TEACHER'){
+            $subjectList = $this->getUser()->getTaughtSubjects();
+        }else{
+            $subjectList = $em->getRepository('App:Subject')->findAll();
+            if ($this->getUser()->getMaxRole() === 'ROLE_USER'){
+                foreach ($subjectList as $subject){
+                    /** @var $subject Subject */
+                    if ($subject->hasStudent($this->getUser())){
+                        $arrayFormAssignment[$subject->getId()] = $this->createUnassignForm($subject);
+                    }else{
+                        $arrayFormAssignment[$subject->getId()] = $this->createAssignForm($subject);
+                    }
+                }
+            }
         }
+
+        return $this->render(':views/subject:index.html.twig', array(
+            'subjectList'     => $subjectList,
+            'assignmentForms' => $arrayFormAssignment
+        ));
     }
 
     /**
@@ -60,4 +74,77 @@ class CommonController extends Controller
         return $subject;
     }
 
+
+    /**
+     * assign a student to a subject.
+     *
+     * @Route("/assign/to/subject/{id}", name="subject_assign")
+     * @Method("POST")
+     */
+    public function assignToSubjectAction(Request $request, Subject $subject)
+    {
+        $form = $this->createAssignForm($subject);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $subject->addStudent($this->getUser());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($subject);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('subject_index');
+    }
+
+    /**
+     * assign a student to a subject.
+     *
+     * @Route("/unassign/to/subject/{id}", name="subject_assign")
+     * @Method("POST")
+     */
+    public function unassignToSubjectAction(Request $request, Subject $subject)
+    {
+        $form = $this->createUnassignForm($subject);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $subject->removeStudent($this->getUser());
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($subject);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('subject_index');
+    }
+
+    //todo these two form creation should be in a service, but no time left.
+    /**
+     * Creates a form to assign a user to a subject entity.
+     *
+     * @param Subject $subject The subject entity
+     *
+     * @return \Symfony\Component\Form\FormInterface The form
+     */
+    private function createAssignForm(Subject $subject)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('subject_assign', array('id' => $subject->getId())))
+            ->setMethod('POST')
+            ->getForm()
+            ;
+    }
+
+    /**
+     * Creates a form to delete a ticket entity.
+     *
+     * @param Subject $subject The subject entity
+     *
+     * @return \Symfony\Component\Form\FormInterface The form
+     */
+    private function createUnassignForm(Subject $subject)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('subject_unassign', array('id' => $subject->getId())))
+            ->setMethod('POST')
+            ->getForm()
+            ;
+    }
 }
